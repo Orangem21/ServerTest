1b3b305dee1a30636ac2a9d8383892d46fe93f42
OrangeM21 <orangemtony@gmail.com>
2019-02-27 23:28:24 +0800

    fix

--- test.js
+++ test.js
@@ -1,61 +1,61 @@
 var io = require('socket.io-client')
 
 const socket = io('ws://192.168.1.74:8088/ssh', {
-			  path: '/socket.io'
-			}
-		);
-socket.on('connect', function(){
-			console.log('connect to jms')
-			
-		socket.emit("token",{
-			        'token': token, 'secret': uuid.v4(),
-			        'size': [100, 140]
-			      });
-		var tick = setInterval(function(){
+	path: '/socket.io'
+}
+);
+socket.on('connect', function () {
+	console.log('connect to jms')
 
-			if(room !=null){
-				console.log('count :' + count)
-				
-				if(count<6){
+	socket.emit("token", {
+		'token': token, 'secret': uuid.v4(),
+		'size': [100, 140]
+	});
+	var tick = setInterval(function () {
+
+		if (room != null) {
+			console.log('count :' + count)
+
+			if (count < 6) {
 				console.log('wait to connect :')
 
-				}else{
-				
+			} else {
+
 				// var processId = 1;
 				var cmd = randomCmd()
-				console.log( "process :" + processId + ' send cmd :' + cmd)
+				console.log("process :" + processId + ' send cmd :' + cmd)
 
-				socket.emit("data",{
-			        'room': room,
-			        'data': '\x03\x0d'
-			      });
+				socket.emit("data", {
+					'room': room,
+					'data': '\x03\x0d'
+				});
 
-				socket.emit("data",{
-			        'room': room,
-			        'data': cmd
-			      });
-				socket.emit("data",{
-			        'room': room,
-			        'data': '\x0d'
-			      });
-				
-				}
-				
-			}else{
-				console.log('room is  null ')
-				
+				socket.emit("data", {
+					'room': room,
+					'data': cmd
+				});
+				socket.emit("data", {
+					'room': room,
+					'data': '\x0d'
+				});
+
 			}
-		},5000);
 
-		
+		} else {
+			console.log('room is  null ')
 
-		});
+		}
+	}, 5000);
 
-		socket.on('data',function(data){
-			
-			count++;
-			var newroom = data['room'];
 
-			if(room != newroom)
-				room = newroom
-		})
+
+});
+
+socket.on('data', function (data) {
+
+	count++;
+	var newroom = data['room'];
+
+	if (room != newroom)
+		room = newroom
+})
